949e2217a97a8a1f19f496b19c7a75481085fb03
muhammed navas <muhammednavas6287@gmail.com>
2024-12-31 02:51:13 +0530

    add the api in delete page

--- src/components/DeleteUniversity.jsx
+++ src/components/DeleteUniversity.jsx
@@ -9,7 +9,7 @@ import { CheckIcon } from "@heroicons/react/24/outline";
 import axios from "axios";
 
 
-const BACKEND_URL = "https://pbs-1-8vqs.onrender.com";
+const BACKEND_URL = "https://pbs-2-steh.onrender.com";
 export  const DeleteUniversity = ({
   setDeleteUniversityHandle,
   deleteID,
